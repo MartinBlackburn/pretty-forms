e8d7b2adb30910c8c8ba1aabb1615eafd0602dcd
Martin Blackburn <walking_fishy@hotmail.com>
2012-11-07 12:59:06 +0000

    added js for checkboxes and radio buttons

--- forms.js
+++ forms.js
@@ -1,24 +1,74 @@
-PrettyCheckbox = function(element) 
+PrettyCheckbox = function(realCheckbox) 
 {   
     //hide element
+    realCheckbox.hide();
+    
+    //if selected set selected class
+    if(realCheckbox.is(":checked")) {
+        var checkboxClass = "checkbox selected"
+    } else {
+        var checkboxClass = "checkbox"
+    }
     
     //replace element with a div with a checkbox class
+    $('<div/>', {
+        "class": checkboxClass,
+        click: function() {
+            toggleSelected($(this));
+        }
+    }).insertAfter(realCheckbox);
     
-    //update hidden element on click
-    
-    //update div selected status
+    //toggle selected status
+    function toggleSelected(fakeCheckbox)
+    {
+        if(realCheckbox.is(":checked")) 
+        {
+            fakeCheckbox.removeClass("selected");
+            realCheckbox.removeAttr("checked");
+        } else {
+            fakeCheckbox.addClass("selected");
+            realCheckbox.attr("checked", "checked");
+        }
+    }
 };
 
 
-PrettyRadio = function(element) 
+PrettyRadio = function(realRadio) 
 {   
-    //hide element
+  //hide element
+    realRadio.hide();
     
-    //replace element with a div with a radio class
+    //if selected set selected class
+    if(realRadio.is(":checked")) {
+        var radioClass = "radio selected"
+    } else {
+        var radioClass = "radio"
+    }
     
-    //update hidden element on click (removing from others with same name)
+    //replace element with a div with a checkbox class
+    $('<div/>', {
+        "class": radioClass,
+        click: function() {
+            toggleSelected($(this));
+        }
+    }).insertAfter(realRadio);
     
-    //update div selected status
+    //toggle selected status
+    function toggleSelected(fakeRadio)
+    {
+        if(!realRadio.is(":checked")) 
+        {
+            //deselect all others with same name
+            var name = realRadio.attr("name");
+            
+            realRadio.closest("form").find("input[type='radio'][name='" + name + "']").removeAttr("checked");
+            realRadio.closest("form").find(".radio").removeClass("selected");
+            
+            //select this one
+            fakeRadio.addClass("selected");
+            realRadio.attr("checked", "checked");
+        }
+    }
 };
 
 PrettySelect = function(element) 
